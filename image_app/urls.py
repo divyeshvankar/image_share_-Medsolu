aec9c42acdbee870076648bc99778b8611c3a131
Divyesh Vankar <divyeshvankar.iitg@gmail.com>
2023-07-14 14:44:11 +0530

    change in some urls

--- image_app/urls.py
+++ image_app/urls.py
@@ -4,10 +4,10 @@ from image_app.views import CustomLoginView,send_image
 
 urlpatterns = [
     path('upload/', views.upload_image, name='upload_image'),
-    path('', views.image_list, name='image_list'),
+    path('images/', views.image_list, name='image_list'),
     path('delete/<int:image_id>/', views.delete_image, name='delete_image'),
     path('scan/', views.scan_photo, name='scan_photo'),
-    path('login/', CustomLoginView.as_view(), name='login'),
+    path('', CustomLoginView.as_view(), name='login'),
     path('signup/', views.signup, name='signup'),
     # path('images/send/<int:image_id>/', send_image, name='send_image'),
      path('images/send/<int:image_id>/form/', views.send_image_form, name='send_image_form'),
--- image_share/urls.py
+++ image_share/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('images/', include('image_app.urls')),
+    path('', include('image_app.urls')),
 ]
